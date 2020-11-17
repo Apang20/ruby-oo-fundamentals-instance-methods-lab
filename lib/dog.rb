7950cbd2b45cd2c1eaab4a1fecf5f7fb670bc5c8
github username <github email address>
2020-11-17 14:41:33 -0600

    commit tree 7717822ad53c926c2786c2817fc126b825a4a586

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,36 +13,28 @@ fido.sit
 
 
 
-# class Dog
+class Dog
 
-#     def name=(dogs_name)
-#       @this_dogs_name = dogs_name
-#     end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
   
-#     def name
-#       @this_dogs_name
-#     end
-#   end
+    def name
+      @this_dogs_name
+    end
+
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    def breed
+      @breed 
+    end
+    
+  end
   
 #   lassie = Dog.new
 #   lassie.name = "Lassie"
   
 #   puts lassie.name
 
-#   class Dog
-#     def initialize(breed)
-#       @breed = breed
-#     end
-  
-#     def breed=(breed)
-#       @breed = breed
-#     end
-  
-#     def breed
-#       @breed
-#     end
-#   end 
-
-
-  
-    
--- lib/person.rb
+++ lib/person.rb
@@ -6,8 +6,46 @@ class Person
     def walk
         puts "The Person is walking"
     end 
-
-    # ada = Person.new
-    # ada.talk
-    # ada.walk 
 end 
+
+class Person
+    def name=(persons_name)
+      @this_persons_name = persons_name
+    end
+  
+    def name
+      @this_persons_name
+    end
+
+    def job=(persons_job)
+        @this_persons_job = persons_job
+    end
+
+    def job 
+        @this_persons_job
+  end
+end 
+  
+  linda = Person.new
+  linda.name = "Linda"
+  
+  puts linda.name
+
+#   class Person
+
+#     def initialize(first_name, last_name)
+#       @first_name = first_name
+#       @last_name = last_name
+#     end
+  
+#     def name=(full_name)
+#       first_name, last_name = full_name.split
+#       @first_name = first_name
+#       @last_name = last_name
+#     end
+  
+#     def name
+#       "#{@first_name} #{@last_name}".strip
+#     end
+  
+#   end 
