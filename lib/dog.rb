7ae11cac7b365a5c24abb1dda99478ac01303a63
github username <github email address>
2020-11-16 20:10:16 -0600

    instanceVar

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,21 @@ end
 fido = Dog.new 
 fido.bark
 fido.sit 
+
+
+
+class Dog
+
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
